3c7691365479f63133d3a610615d7ab5d02a4652
jhw510 <jhw510@hanmail.net>
2020-05-04 09:34:29 +0900

    200502

---
+++ java_study/src/java_study/Gugudan.java
@@ -0,0 +1,12 @@
+package java_study;
+
+public class Gugudan {
+public static void main(String[] args) {
+	for(int i=1;i<10;i++) {
+		System.out.println();
+		for(int j=2;j<10;j++) {
+			System.out.println(i+"*"+j+"="+(j*i));
+		}
+	}
+}
+}
